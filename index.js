d5f73305489268722d51ed28c7ecee2be5646834
Matthew Clancy <mmclancy01@yahoo.com>
2024-02-09 13:03:40 -0500

    image displaying

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ fetch("https://api.fbi.gov/wanted/v1/list")
 .then((response) => response.json())
 .then((wanteds) => {
     for (let wanted of wanteds.items) {
-        console.log(wanted.title)
+        let fugitiveName = wanted.title
+        let fugitiveImage = wanted.images[0].thumb
+        // console.log()
+        console.log(fugitiveName)
+        console.log(fugitiveImage)
+        let imageContainer = document.querySelector("#real-fugitives")
+        let imgElement = document.createElement('img')
+        imgElement.src = fugitiveImage
+        imageContainer.append(imgElement)
     }
 })
-
+// function fetchFugitive() {
+//     return fetch("https://api.fbi.gov/wanted/v1/list")
+// .then((response) => response.json())
+// .then((wanteds) => {
+//    return wanteds
+//     })
+// }
+// const displayFugitive = () => {
+//     fetchFugitive().then((items) => {
+//         console.log(items)
+//     }
+//     )
+// }
