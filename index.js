4adc00da40241981590195c3ec1637cfd69910fc
Matthew Clancy <mmclancy01@yahoo.com>
2024-02-08 19:24:22 -0500

    cleaning up

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// async function getMostWanted(){
-//     const response = await fetch("https://api.fbi.gov/wanted/v1/list");
-//     const mostWanted = await response.json();
-//     console.log(mostWanted);
-// }
-
-// getMostWanted();
-
-
 fetch("https://api.fbi.gov/wanted/v1/list")
 .then((response) => response.json())
 .then((wanteds) => {
