6dfb4585af38a5e1bf8e67d81e0d2a3ffff3354c
seth spring <seth.c.spring@gmail.com>
2024-02-12 16:36:55 -0500

    added form submit button and alert in index.js

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-const fetchUrl = ("https://api.fbi.gov/wanted/v1/list")
+const fetchUrl = "https://api.fbi.gov/wanted/v1/list";
 
 fetch(fetchUrl)
-.then((response) => response.json())
-.then((wanteds) => {
+  .then((response) => response.json())
+  .then((wanteds) => {
     for (let wanted of wanteds.items) {
-        let fugitiveName = wanted.title
-        let fugitiveImage = wanted.images[0].thumb
-        console.log(fugitiveName)
-        console.log(fugitiveImage)
-        let imageContainer = document.querySelector("#real-fugitives")
-        let imgElement = document.createElement('img')
-        imgElement.src = fugitiveImage
-        imageContainer.append(imgElement)
-        // fugitiveImage.splice()
-        imgElement.addEventListener('click', () => {
-            handleClick(wanted)
-        })
-
+      let fugitiveName = wanted.title;
+      let fugitiveImage = wanted.images[0].thumb;
+      console.log(fugitiveName);
+      console.log(fugitiveImage);
+      let imageContainer = document.querySelector("#real-fugitives");
+      let imgElement = document.createElement("img");
+      imgElement.src = fugitiveImage;
+      imageContainer.append(imgElement);
+      // fugitiveImage.splice()
+      imgElement.addEventListener("click", () => {
+        handleClick(wanted);
+      });
     }
-})
+  });
 
 function handleClick(wanted) {
-   let nameClass = document.querySelector(".name")
-//    let crimeClass = document.querySelector(".crime")
-//    let rewardClass = document.querySelector(".reward")
-   let imageClass = document.querySelector(".detail-image")
-   nameClass.textContent= wanted.title
-   imageClass.src = wanted.images[0].original
-   
-} 
+  let nameClass = document.querySelector(".name");
+  //    let crimeClass = document.querySelector(".crime")
+  //    let rewardClass = document.querySelector(".reward")
+  let imageClass = document.querySelector(".detail-image");
+  nameClass.textContent = wanted.title;
+  imageClass.src = wanted.images[0].original;
+}
+
+// select the form element
+let submitFormButton = document.querySelector("#submit-button");
+// listen for the submit event on the form
+submitFromButtom.addEventListener("submit", () => {
+  event.preventDefault(); //
+  alert("Faux Fugitive Added to Database ");
+});
+
 // function fetchFugitive() {
 //     return fetch("https://api.fbi.gov/wanted/v1/list")
 // .then((response) => response.json())
